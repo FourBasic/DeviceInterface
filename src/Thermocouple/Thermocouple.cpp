7aaebf475727bb3d3be638c7c80694fa6bcbf8d9
FourBasic <alex.dunn@live.ca>
2026-02-23 21:48:44 -0500

    check limits + fault

--- src/Thermocouple/Thermocouple.cpp
+++ src/Thermocouple/Thermocouple.cpp
@@ -3,7 +3,7 @@
 
 Thermocouple::Thermocouple() { }
 
-void Thermocouple::setup(uint8_t _type, uint8_t _decimal) {    
+void Thermocouple::setup(uint8_t _type, uint8_t _decimal, int _faultLimit_L, int _faultLimit_H) {    
     type = _type;
     decimal = _decimal;
 }
@@ -26,7 +26,7 @@ int Thermocouple::update(int _uVolt) {
     } else {
         float m;
         switch (type) {
-            case TYPE_K:      
+            case TC_TYPE_K:      
                 m = (10.0 + (element * 10.0)) / (uv_per_10C_K[element]);
                 tempC = uVolt * m;
                 break;
@@ -36,6 +36,22 @@ int Thermocouple::update(int _uVolt) {
                 break;
         }
     }
-    
+    checkLimits();
     return int(tempC * (1 + decimal));
+}
+
+bool Thermocouple::checkLimits() { 
+    if (tempC > faultLimit_H) { 
+        faultCode = TC_FAULT_HIGH_LIMIT;
+        return false;
+    } else if (tempC < faultLimit_L) {
+        faultCode = TC_FAULT_LOW_LIMIT;
+        return false;
+    } else if (faultCode == TC_FAULT_HIGH_LIMIT || faultCode == TC_FAULT_LOW_LIMIT) {
+        faultCode = 0;
+    }    
+}
+
+int Thermocouple::getFaultCode() { 
+    return faultCode;
 }
--- src/Thermocouple/Thermocouple.h
+++ src/Thermocouple/Thermocouple.h
@@ -1,18 +1,24 @@
 #ifndef THERMOCOUPLE_H
 #define THERMOCOUPLE_H
-#define TYPE_K 0
+#define TC_TYPE_K 0
+#define TC_FAULT_HIGH_LIMIT 1
+#define TC_FAULT_LOW_LIMIT 2
 #include <Arduino.h>
 
 class Thermocouple {
   public:		
 	  Thermocouple();
-    void setup(uint8_t _type, uint8_t _decimal);
+    void setup(uint8_t _type, uint8_t _decimal, int _faultLimit_L, int _faultLimit_H);
     int update(int _in);
+    bool checkLimits();
+    int getFaultCode();
   private:
     int type;
     int decimal;
     int uVolt;
     float tempC;
+    int faultLimit_L, faultLimit_H;
+    int faultCode;
     const int uv_per_10C_K[25] = {397,798,1203,1612,2023,2436,2851,3267,3682,4096,4509,4920,5328,5735,6138,6540,6941,7340,7739,8138,8539,8940,9343,9747,10153};
 };
 #endif
